63a118216621ed26c3aa7d7757339404dad7477e
fuhuicong <fu_huicong@qq.com>
2019-11-18 08:53:18 +0800

    Initial Commit

---
+++ JavaScriptCoreDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JavaScriptCoreDemo
+//
+//  Created by fuhuicong-yfzx on 2019/11/18.
+//  Copyright © 2019 fuhuicong-yfzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JavaScriptCoreDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JavaScriptCoreDemo
+//
+//  Created by fuhuicong-yfzx on 2019/11/18.
+//  Copyright © 2019 fuhuicong-yfzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
